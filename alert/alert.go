b78e80e47ad4cd7cf24cb9128c36a5b0a1bb024c
Michael MacDonald <michael.macdonald@intel.com>
2016-02-10 13:50:38 -0500

    Add logging/alert package
    An alert.Logger implements the following functions:
      Output(int, string)
      Warn(...interface{})
      Warnf(string, ...interface{})
      Fatal(...interface{})
      Fatalf(string, ...interface{})
    
    By default, messages are emitted to os.Stderr, but custom alert.Loggers
    may be implemented (e.g. to send messages to syslog, etc).

---
+++ alert/alert.go
@@ -0,0 +1,93 @@
+package alert
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type (
+	// Logger defines an interface for an alert logger
+	Logger interface {
+		Output(int, string)
+
+		Warn(...interface{})
+		Warnf(string, ...interface{})
+
+		Fatal(...interface{})
+		Fatalf(string, ...interface{})
+	}
+
+	// StdErrLogger is a logger which writes output to os.Stderr
+	StdErrLogger struct {
+		log *log.Logger
+	}
+)
+
+var std Logger
+
+// Provide as much information as possible about where the message originated,
+// as this package should usually only be involved where there is a failure.
+const logFlags = log.Ldate | log.Ltime | log.LUTC | log.Llongfile
+
+func init() {
+	std = NewStdErrLogger()
+}
+
+// NewStdErrLogger returns a *StdErrLogger
+func NewStdErrLogger() *StdErrLogger {
+	return &StdErrLogger{
+		log: log.New(os.Stderr, "ALERT ", logFlags),
+	}
+}
+
+// Output writes the output for a logging event
+func (l *StdErrLogger) Output(skip int, s string) {
+	l.log.Output(skip, s)
+}
+
+// Warn outputs a log message from the arguments
+func (l *StdErrLogger) Warn(v ...interface{}) {
+	l.Output(3, fmt.Sprint(v...))
+}
+
+// Warnf outputs a formatted log message from the arguments
+func (l *StdErrLogger) Warnf(f string, v ...interface{}) {
+	l.Output(3, fmt.Sprintf(f, v...))
+}
+
+// Fatal outputs a log message from the arguments, then exits
+func (l *StdErrLogger) Fatal(v ...interface{}) {
+	l.Output(3, fmt.Sprint(v...))
+	os.Exit(1)
+}
+
+// Fatalf outputs a formatted log message from the arguments, then exits
+func (l *StdErrLogger) Fatalf(f string, v ...interface{}) {
+	l.Output(3, fmt.Sprintf(f, v...))
+	os.Exit(1)
+}
+
+// package-level functions follow
+
+// Warn outputs a log message from the arguments
+func Warn(v ...interface{}) {
+	std.Output(3, fmt.Sprint(v...))
+}
+
+// Warnf outputs a formatted log message from the arguments
+func Warnf(f string, v ...interface{}) {
+	std.Output(3, fmt.Sprintf(f, v...))
+}
+
+// Fatal outputs a log message from the arguments, then exits
+func Fatal(v ...interface{}) {
+	std.Output(3, fmt.Sprint(v...))
+	os.Exit(1)
+}
+
+// Fatalf outputs a formatted log message from the arguments, then exits
+func Fatalf(f string, v ...interface{}) {
+	std.Output(3, fmt.Sprintf(f, v...))
+	os.Exit(1)
+}
