2ed637c122cc6d89a1bff2285ce1cee82045008b
Robert Read <robert.read@intel.com>
2016-07-26 17:48:56 -0700

    Fix for darwin pt 2

--- applog/termios.go
+++ applog/termios.go
@@ -1,25 +1,9 @@
-// +build linux
-
 package applog
 
 import (
 	"io"
 	"os"
-	"syscall"
-	"unsafe"
 )
-
-// These constants are declared here, rather than importing
-// them from the syscall package as some syscall packages, even
-// on linux, for example gccgo, do not declare them.
-const ioctlReadTermios = 0x5401  // syscall.TCGETS
-const ioctlWriteTermios = 0x5402 // syscall.TCSETS
-
-func isTerminal(fd uintptr) bool {
-	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
-}
 
 // WriterIsTerminal returns true if the given io.Writer converts to
 // an *os.File and the file's fd is a terminal.
